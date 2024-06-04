b352dafb0173f306e71c879f15587300e6ea351e
junghoon-luckyhuni <luckyhuni@gmail.com>
2024-06-04 14:44:01 +0900

    Update HomeScreen.kt
    feat: Now paly section의 pager cube animation 적용

--- feature/home/src/main/java/com/junghoon/movie/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/junghoon/movie/feature/home/HomeScreen.kt
@@ -1,45 +1,63 @@
 package com.junghoon.movie.feature.home
 
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.junghoon.movie.core.domain.model.Movie
 import com.junghoon.movie.core.ui.base.ErrorState
 import com.junghoon.movie.core.ui.component.AnimatedLoadingView
-import com.junghoon.movie.core.ui.component.LikeIcon
 import com.junghoon.movie.core.ui.component.MoviePoster
-import com.junghoon.movie.core.ui.component.NetworkImage
 import com.junghoon.movie.core.ui.theme.MovieTheme
-import com.junghoon.movie.core.ui.theme.PaleGray
 import kotlinx.collections.immutable.PersistentList
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlin.math.absoluteValue
+import kotlin.math.min
 
 @Composable
 internal fun HomeRoute(
@@ -166,6 +184,9 @@ fun TopRatedSection(movies: PersistentList<Movie>, onMovieClick: (Int) -> Unit, 
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+fun PagerState.offsetForPage(page: Int) = (currentPage - page) + currentPageOffsetFraction
+
 @Composable
 private fun NowPlaySection(
     movies: PersistentList<Movie>,
@@ -177,24 +198,95 @@ private fun NowPlaySection(
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(
-                items = movies,
-                key = { movie ->
-                    movie.id
-                }
+        AutoScrollNowPlaySection(movies = movies, onMovieClick = onMovieClick, onLikeClick = onLikeClick)
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun AutoScrollNowPlaySection(movies: PersistentList<Movie>, onMovieClick: (Int) -> Unit, onLikeClick: (Int, Boolean) -> Unit) {
+    val deg = 105f
+
+    val pagerState = rememberPagerState()
+
+    LaunchedEffect(key1 = Unit) {
+        while (isActive) {
+            runCatching {
+                delay(2000)
+                pagerState.animateScrollToPage(
+                    page = (pagerState.currentPage.inc()) % Integer.MAX_VALUE,
+                    animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing)
+                )
+            }
+        }
+    }
+
+    val scale by remember {
+        derivedStateOf {
+            1f - (pagerState.currentPageOffsetFraction.absoluteValue) * 0.3f
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val offsetFromStart = pagerState.offsetForPage(0).absoluteValue
+        Box(
+            modifier = Modifier
+                .aspectRatio(1f)
+                .offset { IntOffset(0, 160.dp.roundToPx()) }
+                .scale(scaleX = 0.6f, scaleY = 0.24f)
+                .scale(scale)
+                .rotate(offsetFromStart * 90f)
+                .blur(
+                    radius = 110.dp,
+                    edgeTreatment = BlurredEdgeTreatment.Unbounded,
+                )
+                .background(Color.White.copy(alpha = 0.5f))
+        )
+
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            pageCount = Integer.MAX_VALUE,
+            state = pagerState
+        ) { page ->
+            Box(
+                modifier = Modifier
+                    .aspectRatio(0.7f)
+                    .graphicsLayer {
+                        val pageOffset = pagerState.offsetForPage(page)
+                        val offScreenRight = pageOffset < 0f
+
+                        val interpolated = FastOutLinearInEasing.transform(pageOffset.absoluteValue)
+                        rotationY = min(interpolated * if (offScreenRight) deg else -deg, 90f)
+
+                        transformOrigin = TransformOrigin(
+                            pivotFractionX = if (offScreenRight) 0f else 1f,
+                            pivotFractionY = 0.5f
+                        )
+                    }
+                    .drawWithContent {
+                        this.drawContent()
+
+                        drawRoundRect(
+                            color = Color.Black.copy((pagerState.offsetForPage(page).absoluteValue * 0.7f)),
+                            cornerRadius = CornerRadius(10.dp.toPx(), 10.dp.toPx())
+                        )
+                    }
             ) {
+                val movie = movies[page % movies.size]
+
                 MoviePoster(
                     modifier = Modifier
-                        .width(150.dp)
-                        .height(200.dp),
-                    poster = it.posterPath,
-                    isLike = it.isLike,
+                        .fillMaxSize(),
+                    poster = movie.posterPath,
+                    isLike = movie.isLike,
                     onMovieClick = {
-                        onMovieClick(it.id)
+                        onMovieClick(movie.id)
                     },
                     onLikeClick = {
-                        onLikeClick(it.id, !it.isLike)
+                        onLikeClick(movie.id, !movie.isLike)
                     }
                 )
             }
